148a7a69436d6ef5ade923c6320277fd91a8c2df
Vedprakash321 <131158289+Vedprakash321@users.noreply.github.com>
2023-08-30 01:59:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
 //your JS code here. If required.
-it('should open lightbox for images', () => {
-  cy.visit('/path-to-your-page'); // Replace with the actual path
-
-  // Click on the first image to open the lightbox
-  cy.get('[data-lightbox="gallery"]:first').click();
-
-  // You can add assertions here to check for lightbox elements
-  // For example:
-  cy.get('div#lightbox.lightbox').should('have.length.at.least', 1);
-});
